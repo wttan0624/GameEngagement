92f6f02544456441317cce0b1829f117ce130e12
wttan0624 <wttan-wm23@student.tarc.edu.my>
2025-09-07 15:13:08 +0800

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -3,7 +3,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
 from xgboost import XGBClassifier
 
 
@@ -101,5 +100,6 @@ if st.sidebar.button("Predict Engagement"):
     prediction = model.predict(X_input)[0]
     engagement_levels = {0: "Low", 1: "Medium", 2: "High"}  # adjust to your encoding
     st.metric("Predicted Engagement Level", engagement_levels[prediction])
+
 
 
